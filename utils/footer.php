cc02766f0a44a5094c1965607555aa910fee8fa3
Aishwarya Iyer <74247717+Aishwarya2701@users.noreply.github.com>
2023-12-23 12:13:26 +0530

    Add files via upload

---
+++ utils/footer.php
@@ -0,0 +1,37 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>A</strong><span class = "small footerSubtext">a</span>
+                        <strong><B></B></strong><span class = "small footerSubtext">b</span>
+                        <strong>C</strong><span class = "small footerSubtext">c</span>
+                        <strong>D</strong><span class = "small footerSubtext">d</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                    abcdefg
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                <p> <br>
+                 Address: Thane West
+                    <br>Thane,Maharashtra 400610 </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                    <a href="https://www.facebook.com/abc/"><img src = "images/facebook.png"></a>
+                    <a href="https://www.instagram.com/abc/"><img src = "images/instagram.png" width="65px"></a>
+                        
+                        <a href="https://www.youtube.com/channel/ddcfddf"><img src = "images/youtube.png"></a>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
